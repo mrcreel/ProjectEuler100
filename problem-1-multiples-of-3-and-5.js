7e40652bc71efd103d489647b96e3661dfb5e617
mrcreel <michaelrcreel@gmail.com>
2020-01-04 07:40:49 -0600

    Completed Challenge 1

---
+++ problem-1-multiples-of-3-and-5.js
@@ -0,0 +1,12 @@
+function multiplesOf3and5(number) {
+  // Good luck!
+    let total = 0
+  for(let i=3; i<number; i++){
+    if (i%3==0||i%5==0){
+      total+=i
+    }
+  }
+  return total;
+}
+
+multiplesOf3and5(1000);

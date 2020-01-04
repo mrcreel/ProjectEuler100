6fb344cbd1d66baeb9bbccfa0316841e0fb696f7
mrcreel <michaelrcreel@gmail.com>
2020-01-04 15:53:16 -0600

    Remove commented out  console.logs and description

--- problem-4-largest-palindrome-product.js
+++ problem-4-largest-palindrome-product.js
@@ -1,10 +1,3 @@
-/*
-Project Euler: Problem 4: Largest palindrome product
-A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-
-Find the largest palindrome made from the product of two n-digit numbers.
- */
-
 function largestPalindromeProduct(n) {
   // Good luck!
   const low = 10 ** (n - 1)
@@ -22,28 +15,7 @@ function largestPalindromeProduct(n) {
       }
     }
   }
-  console.log(largestPalindrom)
   return largestPalindrom
 }
 
-largestPalindromeProduct(3)
-
-/*
-largestPalindromeProduct(2) should return 9009.
-largestPalindromeProduct(3) should return 906609.
-*/
-
-/*
-    for (let h = low; h <= high; h++) {
-      let product = l * h
-
-
-      const reversed_prod = parseInt(prod_string.reverse().join(''))
-      console.log(`${l} X ${high} = ${product} Reversed is: ${reversed_prod}`)
-      /*
-      if (product == reversed_prod) {
-        console.log(`${l} X ${high} = ${product} Reversed is: ${reversed_prod}`)
-      }
-
-    }
-    */
+largestPalindromeProduct(3) // returns 906609
